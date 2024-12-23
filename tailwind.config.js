dcea7989dcecf521f10ff9a54305a9c113c4458e
dnsque <126558330+dnsque@users.noreply.github.com>
2024-12-23 08:37:05 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      textColor: {
+        primary: '#0A6972', 
+      },
+    },
+  },
+  plugins: [],
+};
